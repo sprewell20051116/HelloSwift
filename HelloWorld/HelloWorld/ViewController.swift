ed6f6627dfd0be303ecb2d236327f2fd7c41af6f
GIGIGUN <sprewell20051116@gmail.com>
2016-10-04 19:58:25 +0800

    Init HelloSwift

---
+++ HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by GIGIGUN on 04/10/2016.
+//  Copyright © 2016 GIGIGUN. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
